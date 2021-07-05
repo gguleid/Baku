d32213c5bc89e992e12b87b634e470b0cb640039
gguleid <gguleid@gmail.com>
2021-07-05 15:49:02 -0400

    adds link to backend, create route & functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Index from "../pages/Index";
 import Show from "../pages/Show";
 
 function Main(props){
+    const [ products, setProducts ] = useState(null);
+
+    const URL ="https://dry-savannah-59356.herokuapp.com/";
+
+    getProducts = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setProducts(data);
+    };
+
+    createProducts = async (product) => {
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(product),
+        });
+        getProducts();
+    };
+    
+    useEffect(() => getProducts(), []);
+
     return (
     <div>
         <Switch>
             <Route exact path="/">
-                <Index />
+                <Index products={products} createProducts={createProducts} />
             </Route>
             <Route 
                 path="/products/:id"
