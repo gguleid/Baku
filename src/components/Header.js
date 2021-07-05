977f83b470c8b30e0c9a22ad14ccd84e4840e8eb
gguleid <gguleid@gmail.com>
2021-07-05 15:04:42 -0400

    sets up Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
-function Header(props){
-    return <h1>Header</h1>
+import { Link } from "react-router-dom";
+
+function Header(props) {
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <div>Baku Beverage App</div>
+      </Link>
+    </nav>
+  );
 }
 
 export default Header;
